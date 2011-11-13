83527f3ff6a9414108ed7b53ddbddc430a78ac39
Ryan Bigg <radarlistener@gmail.com>
2011-11-13 12:55:42 +1100

    Add SearchEngine 'class' and iPhone styling

--- sume.js
+++ sume.js
@@ -4,21 +4,18 @@ Sume = {
   }
 }
 
-Sume.SearchController = {
- search: function(term) {
-
- }
-}
-
 Sume.SearchResult = Backbone.Model.extend({})
 Sume.SearchResultList = Backbone.Collection.extend({
   model: Sume.SearchResult
 })
 Sume.searchResults = new Sume.SearchResultList()
 
-Sume.SearchController = {
-  locations: { "ActiveSupport::Concern": "ActiveSupport/Concern.html?callback=?" },
-  search: function(term) {
+Sume.SearchEngine = {
+  locations: { "ActiveSupport::Concern": "ActiveSupport/Concern.html",
+               "distance_of_time_in_words": "ActionView/Helpers/DateHelper/distance_of_time_in_words.html" },
+  search: function() {
+    $('#search_autocomplete').hide();
+    term = $.trim($('#search_autocomplete li.active').text())
     $.get(this.locations[term], function(html) {
       $('#docs').html(html)
     })
@@ -27,20 +24,21 @@ Sume.SearchController = {
 
 Sume.AutoCompleteController = {
   autocomplete_data: ["ActiveSupport::Concern",
-                      "distance_of_time_in_words",
-                      "dandilions"],
+                      "distance_of_time_in_words"],
 
   search: function(term) {
     $('#search_autocomplete').show()
     $('#search_autocomplete').empty()
     var results = $.grep(this.autocomplete_data, function(element) {
-      return element.indexOf(term) != -1;
+      return element.search(new RegExp(term, "i")) != -1;
     })
 
 
     $.each(results, function (index, term) {
       Sume.searchResults.add({term : term})
     })
+
+    $('#search_autocomplete li').first().addClass('active')
   }
 }
 
@@ -81,25 +79,19 @@ Sume.SearchResultView = Backbone.View.extend({
   },
 
   retrieve: function() {
-    $('search_autocomplete').hide();
-    this.options.router.navigate('search/' + $.trim($(this.el).text()), true)
+    Sume.SearchEngine.search();
   }
 })
 
 Sume.SearchRouter = Backbone.Router.extend({
 
   routes: {
-    'search/:term' : 'search',
-    'autocomplete/:part' : 'autocomplete'
+    'autocomplete/:term' : 'autocomplete'
   },
 
   initialize: function() {
     new Sume.SearchView({router : this})
     new Sume.SearchResultsView({router: this});
-  },
-
-  search: function(term) {
-    Sume.SearchController.search(term)
   },
 
   autocomplete: function(term) {
@@ -121,13 +113,14 @@ Sume.SearchView = Backbone.View.extend({
 
   autocomplete: function(e) {
     this.router.navigate('autocomplete/' + $(this.el).val(), true)
-    if (e.keycode == 13) {
-      this.router.navigate('search/' + $(this.el).val(), true);
-    }
   }
 })
 
 $(document).keydown(function(e){
+  if (e.keyCode == 13) {
+    Sume.SearchEngine.search();
+  }
+
   if (e.keyCode == 38) {
     element = $('ul#search_autocomplete .active')
     if (element.length > 0) {
